1cd726b1462a088606474ac1bb8c7bd4957cb1a7
eipi10 <eipi10@qq.com>
2018-11-29 14:22:38 +0800

    add

---
+++ mysql/archive.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+script=$(readlink -f "$0")
+script_path=$(dirname "$script")
+
+clean_time=`date +%Y-%m-%d-%H-%M-%S`
+mkdir -p $script_path/history
+if [ -f $script_path/tpch.csv  ]; then
+  mv -f $script_path/tpch.csv $script_path/history/tpch.csv.$clean_time
+fi
+if [ -f $script_path/tpch.log  ]; then
+  mv -f $script_path/tpch.log $script_path/history/tpch.log.$clean_time
+fi
+echo test_time,database,db_type,query_id,query_time > $script_path/tpch.csv
+echo 'done'
+
+
---
+++ postgresql/run.sh
@@ -0,0 +1,41 @@
+# !/bin/bash
+# ./run.sh aa00 tpch_1g postgresql 
+# ./run.sh aa00 tpch_1g postgresql 3
+
+script=$(readlink -f "$0")
+script_path=$(dirname "$script")
+
+server=${1:-aa00}
+database=${2:-tpch_1g}
+db_type=${3:-postgresql}
+query_id=${4}
+query_path=$script_path/query
+output_path=$script_path/tpch.csv
+log_path=$script_path/tpch.log
+
+TIMEFORMAT=%3R
+
+echo '=========================================================' >> $log_path
+test_time=`date +%Y-%m-%d-%H:%M:%S`
+echo $test_time: start tpc-h | tee -a $log_path
+
+if [ "$query_id" = "" ] ; then
+  i=1
+  n=23
+else
+  i=$query_id
+  n=`expr $query_id + 1`
+fi
+
+while [ $i -lt $n ]
+do
+  echo '-------------------------------------------------------' | tee -a $log_path   
+  echo `date +%Y-%m-%d-%H:%M:%S`: run query $i | tee -a $log_path
+  elasped_time="$(time (PGPASSWORD=tpch psql -h $server -U tpch -d $database -f $query_path/$i.sql >> $log_path) 2>&1 )"
+  echo `date +%Y-%m-%d-%H:%M:%S`: elaspe_time = $elasped_time seconds | tee -a $log_path
+  echo "$test_time,$database,$db_type,$i,$elasped_time" >> $output_path
+  i=`expr $i + 1`
+done
+echo '-------------------------------------------------------' | tee -a $log_path
+echo `date +%Y-%m-%d-%H:%M:%S`: finish tpc-h | tee -a $log_path
+
