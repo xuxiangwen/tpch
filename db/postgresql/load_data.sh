2076d71563f2b189f57210ade50308b624e430b3
eipi10 <eipi10@qq.com>
2018-12-05 23:05:58 +0800

    d

---
+++ db/postgresql/load_data.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+script=$(readlink -f "$0")
+script_path=$(dirname "$script")
+
+source $script_path/tpch.conf
+
+scale_list=${1:-1 3 10 30}
+
+old_path=`pwd`
+tables="region nation supplier customer part partsupp orders lineitem"
+
+for scale in $scale_list
+do
+  echo -----------------------------------------------------------
+  echo `date +%Y-%m-%d-%H:%M:%S`: start load ${scale}g data
+  for table in $tables
+  do
+    echo `date +%Y-%m-%d-%H:%M:%S`: load $table
+    cd $data_path/${scale}g
+    $script_path/sql.sh "\COPY $table FROM $table.csv WITH DELIMITER AS '|';"  tpch_${scale}g 
+  done
+  $script_path/sql_file.sh $script_path/dss.check  tpch_${scale}g 
+  echo `date +%Y-%m-%d-%H:%M:%S`: finish load ${scale}g data
+  echo -----------------------------------------------------------
+done
+
+cd $old_path
