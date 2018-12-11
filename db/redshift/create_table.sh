e7ed2074118b5f7a278925ab2c9f852563c1b87b
eipi10 <eipi10@qq.com>
2018-12-11 02:25:46 +0000

    update

--- db/redshift/create_table.sh
+++ db/redshift/create_table.sh
@@ -5,9 +5,10 @@ script_path=$(dirname "$script")
 source $script_path/tpch.conf
 
 scale_list=${1:-1 3 10 30}
+ddl=${2:-dss.ddl}
 
 for scale in $scale_list
 do
   echo -----------------------------------------------------------
-  $script_path/sql_file.sh $script_path/dss.ddl tpch_${scale}g 
+  $script_path/sql_file.sh $script_path/$ddl tpch_${scale}g 
 done
