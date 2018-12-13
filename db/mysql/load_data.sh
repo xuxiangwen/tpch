b27417a0444063bcdd779b17f627f8c6e781ae33
EC2 Default User <ec2-user@ip-172-31-32-229.cn-northwest-1.compute.internal>
2018-12-13 09:25:40 +0000

    update load_data.sh 1 0.1

--- db/mysql/load_data.sh
+++ db/mysql/load_data.sh
@@ -5,6 +5,7 @@ script_path=$(dirname "$script")
 source $script_path/tpch.conf
 
 scale_list=${1:-1 3 10 30}
+data_scale=${2}
 
 old_path=`pwd`
 tables="region nation supplier customer part partsupp orders lineitem"
@@ -12,14 +13,19 @@ tables="region nation supplier customer part partsupp orders lineitem"
 for scale in $scale_list
 do
   echo -----------------------------------------------------------
-  echo `date +%Y-%m-%d-%H:%M:%S`: start load ${scale}g data
+  if [ "$data_scale" = ""  ]; then
+    real_scale=$scale
+  else
+    real_scale=$data_scale
+  fi
+  echo `date +%Y-%m-%d-%H:%M:%S`: start load ${real_scale}g data
   for table in $tables
   do
     echo `date +%Y-%m-%d-%H:%M:%S`: load $table
-    cd $data_path/${scale}g
+    cd $data_path/${real_scale}g
     $script_path/sql.sh "load data local INFILE '$table.tbl' INTO TABLE $table FIELDS TERMINATED BY '|';"  tpch_${scale}g 
   done
-  echo `date +%Y-%m-%d-%H:%M:%S`: finish load ${scale}g data
+  echo `date +%Y-%m-%d-%H:%M:%S`: finish load ${real_scale}g data
   $script_path/sql_file.sh $script_path/dss.check  tpch_${scale}g 
   echo `date +%Y-%m-%d-%H:%M:%S`: done
   echo -----------------------------------------------------------
